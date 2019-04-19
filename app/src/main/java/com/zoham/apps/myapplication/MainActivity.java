d37e60bae2afb8248d31d89a9b1bdf4ac375a721
Zoham <sohamk97@gmail.com>
2019-04-19 19:33:33 +0530

    Scan and discoverability included

--- app/src/main/java/com/zoham/apps/myapplication/MainActivity.java
+++ app/src/main/java/com/zoham/apps/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,13 +20,19 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothDevice btDevice = null;
     private String btDeviceAddress = null;
 
-    private Set<BluetoothDevice> btDeviceSet;
+    private Set<BluetoothDevice> btDeviceSet = new HashSet<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(btBroadcastReceiver);
     }
 
     public void btInit(View view) {
@@ -48,5 +55,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private final BroadcastReceiver btBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
 
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                btDeviceSet.add(device);
+            }
+        }
+    };
+
+    private void setUpDeviceListView() {
+
+    }
+
+    public void btScan(View view) {
+        btDeviceSet.addAll(btAdapter.getBondedDevices());
+
+
+        if(btAdapter.isDiscovering()) btAdapter.cancelDiscovery();
+
+        setUpDeviceListView();
+
+        IntentFilter foundIntentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(btBroadcastReceiver, foundIntentFilter);
+
+        btAdapter.startDiscovery();
+    }
+
+    public void btDeviceDiscoverable(View view) {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(btAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+    }
 }
